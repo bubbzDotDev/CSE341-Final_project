e632e558dec1ad7c97ee37b8223c8ea39237bfc6
rbridge1104 <35268078+rbridge1104@users.noreply.github.com>
2022-03-17 22:14:39 -0400

    recipe delete not functioning

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const { body } = require('express-validator/check');
+const { body } = require('express-validator');
 
 const recipeController = require('../controllers/recipe');
 
 const router = express.Router();
 
 // PUT request to allow editing on a recipe.
-router.put(
-    'admin/recipe/:recipeId', recipeController.updateRecipe
-);
+// router.put(
+//     'admin/recipe/:recipeId', recipeController.updateRecipe
+// );
 
 router.delete('admin/recipe/:recipeId', recipeController.deleteRecipe);
 module.exports = router;
