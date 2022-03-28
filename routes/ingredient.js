8eb023be327d1ecd80533acb640f25b0ee1f60e4
josh-wassum <joshuawassum@gmail.com>
2022-03-28 11:44:10 -0600

    Added data validation

--- routes/ingredient.js
+++ routes/ingredient.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const { body } = require("express-validator");
 
+const isAuth = require('../middleware/is-auth');
+
 const ingredientController = require('../controllers/ingredient');
 
 const router = express.Router();
@@ -10,15 +12,15 @@ const router = express.Router();
 router.get('/ingredient', ingredientController.getIngredient);
 
 //POST add ingredient
-router.post('/add-ingredient', ingredientController.postAddIngredient);
+router.post('/add-ingredient', isAuth, ingredientController.postAddIngredient);
 
 //GET ingredient by ID
 router.get('/ingredient/:ingredientId', ingredientController.getIngredientById);
 
 //PUT update ingredient
-router.put('/edit-ingredient/:ingredientId', ingredientController.putIngredientById);
+router.put('/edit-ingredient/:ingredientId', isAuth, ingredientController.putIngredientById);
 
 //DELETE ingredient
-router.delete('/delete-ingredient/:ingredientId', ingredientController.deleteIngredientById);
+router.delete('/delete-ingredient/:ingredientId', isAuth, ingredientController.deleteIngredientById);
 
 module.exports = router;
