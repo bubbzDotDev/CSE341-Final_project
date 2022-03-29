d9c5b291ead93557cd72cea0ec318d7713db8ea1
garywilki <wil20223@byui.edu>
2022-03-28 21:23:32 -0700

    fixed conflicts x2

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-<<<<<<< HEAD
     const authHeader = req.get("auth");
     console.log(authHeader);
     if (!authHeader) {
@@ -11,10 +10,6 @@ module.exports = (req, res, next) => {
     }
     
     const token = authHeader.split(' ')[1];
-=======
-    const token = req.get('Authorization').split(' ')[1];  // req.get('Authorization') is undefined during testing -Chris
-    
->>>>>>> 6628e1e5f7b456b5f7b58400ed3cd803126bc762
     let decodedToken;
     try {
         decodedToken = jwt.verify(token, 'somesupersecretstring');
